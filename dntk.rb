4d71361e912a4da68e0d698198e37cbd1c9a9f02
nnao45 <n4sekai5y@gmail.com>
2018-06-09 21:06:12 +0900

    Update dntk.rb

--- dntk.rb
+++ dntk.rb
@@ -3,10 +3,10 @@ require "formula"
 class Dntk < Formula
   desc "Command line's Interactive calculator, GNU bc wrapper."
   homepage "https://github.com/nnao45/dntk"
-  url "https://github.com/nnao45/dntk/archive/v1.0.6.tar.gz"
+  url "https://github.com/nnao45/dntk/archive/v1.0.7.tar.gz"
   sha256 "098a5e1bd6b56ac3374898b16fb9be9bb604abf76f74e2ce5cfc1df70341dbfc"
   head "https://github.com/nnao45/dntk.git"
-  version "v1.0.6"
+  version "v1.0.7"
 
   depends_on "go" => :build
   depends_on "glide" => :build
