559d01db52d123a098413febba76a74d10a8f980
nnao45 <n4sekai5y@gmail.com>
2018-08-10 15:55:41 +0900

    Update dntk.rb

--- dntk.rb
+++ dntk.rb
@@ -4,7 +4,7 @@ class Dntk < Formula
   desc "Command line's Interactive calculator, GNU bc wrapper."
   homepage "https://github.com/nnao45/dntk"
   url "https://github.com/nnao45/dntk/archive/v1.0.9.tar.gz"
-  sha256 "a9239ac87ac74284626bef10fc2601034d22889d0424e46636777038783b431c"
+  sha256 "3bddfa5fe48237b4264017d3da41cc976e198cf898248a49182dc0f59bee0ab8"
   head "https://github.com/nnao45/dntk.git"
   version "v1.0.9"
 
