ddb077b05dbecd3c8a610ad0bbc9bb066bffe463
nnao45 <n4sekai5y@gmail.com>
2019-05-29 13:41:28 +0900

    release

--- dntk.rb
+++ dntk.rb
@@ -3,10 +3,10 @@ require "formula"
 class Dntk < Formula
   desc "Command line's Interactive calculator, GNU bc wrapper."
   homepage "https://github.com/nnao45/dntk"
-  url "https://github.com/nnao45/dntk/archive/v2.0.9.tar.gz"
-  sha256 "f6e4fad0876c43367b5f0be8cf45940780b5ce86705fa428ea8ca32454d9f367"
+  url "https://github.com/nnao45/dntk/archive/v2.1.0.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   head "https://github.com/nnao45/dntk.git"
-  version "v2.0.9"
+  version "v2.1.0"
 
   depends_on "rust" => :build
 
